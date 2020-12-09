45519a3d9a5f689faa3c6d8c23d2ec54abfd5af6
Simon <brehm.simon@gmail.com>
2020-12-09 16:09:31 +0100

    Added initial Catcher. Now it is possible to catch frames and analyze the payload

---
+++ src/BLE_catcher.ino
@@ -0,0 +1,51 @@
+/*
+   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
+   Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
+int scanTime = 5; //In seconds
+BLEScan* pBLEScan;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      if (advertisedDevice.haveServiceUUID() && advertisedDevice.getServiceUUID().toString() == "0000fd6f-0000-1000-8000-00805f9b34fb"){
+        Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+
+        uint8_t* payload = advertisedDevice.getPayload();
+        for (size_t i = 0; i < advertisedDevice.getPayloadLength(); i++) {
+          Serial.printf("iterating through array: %x\n", payload[i]);
+        }
+
+        
+        Serial.printf("PayploadLenght: %zu \n", advertisedDevice.getPayloadLength());
+      }
+      
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Scanning...");
+
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  Serial.print("Devices found: ");
+  Serial.println(foundDevices.getCount());
+  Serial.println("Scan done!");
+  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  delay(20000);
+}
