bd73cecfc5412465d65fdbc27d63eeed5ad9e777
Niklas Schwabe <niklas.schwabe09@gmail.com>
2020-12-14 10:59:51 +0100

    only use 20bytes substring, adds function to write beacons to flash mem

--- src/BLE_catcher/BLE_catcher.ino
+++ src/BLE_catcher/BLE_catcher.ino
@@ -7,9 +7,18 @@
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
+#include <SPIFFS.h>
+
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int scanTime = 5; //In seconds
 BLEScan* pBLEScan;
+
+void writeBeaconToFile(String beacon);
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
@@ -17,11 +26,31 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
         Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
 
         uint8_t* payload = advertisedDevice.getPayload();
-        Serial.printf("Collected raw payload: 0x");
-        for (size_t i = 0; i < advertisedDevice.getPayloadLength(); i++) {
-          Serial.printf("%02x", payload[i]);
+        size_t len = advertisedDevice.getPayloadLength();
+        
+        std::stringstream stream;
+        stream << std::hex << std::setfill('0');
+        
+        // only last 20 bytes of interest; for 31 bytes frame, skip leading 11 bytes; for 28 bytes frame, skip leading 8 bytes
+        for (size_t i = len - 20; i < len; i++) {
+          stream << std::hex << std::setw(2) << (unsigned int) payload[i];
         }
-        Serial.printf("\n");
+
+        // TODO: implement duplicate detection
+        String beacon = stream.str().c_str();
+        //Serial.print("Relevant payload: ");
+        Serial.println(beacon);
+        //writeBeaconToFile(beacon);
+
+        // for testing purposes, can be deleted
+        /*std::stringstream stream2;
+        stream2 << std::hex << std::setfill('0');
+        for (size_t i = 0; i < advertisedDevice.getPayloadLength(); i++) {
+          stream2 << std::hex << std::setw(2) << (unsigned int) payload[i];
+        }
+        String s2 = stream2.str().c_str();
+        Serial.printf("Full payload: ");
+        Serial.println(s2);*/
       }
       
     }
@@ -37,6 +66,18 @@ void setup() {
   pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99);  // less or equal setInterval value
+
+  // setup flash memory and a file to store collected beacons
+  if (!SPIFFS.begin(false)) {
+    Serial.println("Error while initializing SPIFFS!");
+    while (true){}
+  }
+
+  if (!SPIFFS.exists("/beacons.txt")) {
+    File file = SPIFFS.open("/beacons.txt", "w");
+    file.close();
+    Serial.println("File for beacons has been generated.");
+  }
 }
 
 void loop() {
@@ -47,4 +88,14 @@ void loop() {
   Serial.println("Scan done!");
   pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
   delay(20000);
+}
+
+void writeBeaconToFile(String beacon) {
+  File file = SPIFFS.open("/beacons.txt", "a");
+  if (file) {
+    Serial.println("Writing to file: ");
+    Serial.println(beacon);
+    file.println(beacon);
+  }
+  file.close();
 }
