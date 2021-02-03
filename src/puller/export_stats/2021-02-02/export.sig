5f325886c4270c8f071a1e005cdd37917020e79b
Martin Heyden <heyden@Fraunhofer-Mac-Martin.fritz.box>
2021-02-03 21:10:42 +0100

    integrated stats pulling

---
+++ src/puller/export_stats/2021-02-02/export.sig
@@ -0,0 +1,4 @@
+
+‚
+4
+de.rki.coronawarnappv1"262*1.2.840.10045.4.3.2"F0D RàE†M‡Ë…ú>¬,ðT;/0N¢©JG™šsP: 0RgWku	ñ_ ì[æ'	÷vnà°‰­¦ÎBØ
--- src/puller/puller.sh
+++ src/puller/puller.sh
@@ -31,3 +31,17 @@ echo "Filtering encoded temporary exposure keys data"
 tail +17c ./exports/$latest_day/export.bin > ./exports/$latest_day/tek
 
 echo "Done"
+
+echo ""
+
+echo "Downloading stats"
+
+wget https://svc90.main.px.t-online.de/version/v1/stats 2>/dev/null
+
+echo "Unzipping and moving into right directory"
+
+unzip -d ./export_stats/$latest_day stats > /dev/null
+
+rm stats
+
+echo "Done"
