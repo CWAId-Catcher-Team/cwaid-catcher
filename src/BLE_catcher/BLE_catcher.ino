882040ecdc17c4a4e12277d185e4f968334b8446
Niklas Schwabe <niklas.schwabe09@gmail.com>
2021-02-07 21:05:53 +0100

    adds buffering of beacons before writing to file, moves sleep before scan to mitigate potential memory issues

--- src/BLE_catcher/BLE_catcher.ino
+++ src/BLE_catcher/BLE_catcher.ino
@@ -1,8 +1,3 @@
-/*
-   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
-   Ported to Arduino ESP32 by Evandro Copercini
-*/
-
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
@@ -12,7 +7,7 @@
 #include <sstream>
 #include <iostream>
 #include <iomanip>
-//#include <deque>
+#include <vector>
 
 using namespace std;
 
@@ -21,18 +16,23 @@ const uint64_t sleepTime = 24; // pausing scan for 'sleepTime'+1 seconds
 BLEScan* pBLEScan;
 bool scan = true;
 const char * beaconFile = "/beacons.txt";
-//deque<String> seenBeacons;
-//int maxBeacons = 200;
+vector<uint8_t> beaconBuf;
+const int bufSize = 920; // 920 bytes of space for 40 beacons (+3 bytes of metadata each) within one scan interval
 
 
-void writeBeaconToFile(uint8_t* beacon, size_t len);
+void appendBeaconToBuffer(uint8_t* beacon, size_t beaconLen, uint8_t* metadata, size_t metadataLen);
+
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
       if (advertisedDevice.haveServiceUUID() && advertisedDevice.getServiceUUID().equals(BLEUUID((uint16_t) 0xfd6f))){
-        
+
+        // payload of BLE frame
         uint8_t* payload = advertisedDevice.getPayload();
         size_t len = advertisedDevice.getPayloadLength();       
+
+        // metadata
+        uint64_t currentTime = esp_timer_get_time() / 1000000;
         
         int os = 0;
         //TODO store os beacon file
@@ -45,8 +45,14 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
           //ios
           os = 2;
         }
+
+        // 3 bytes can represent 194 days as seconds
+        uint8_t metadata[] = {(currentTime >> 16), (currentTime >> 8), currentTime};
+
+        appendBeaconToBuffer(payload + (len - 20), 20, metadata, sizeof(metadata));
+        
         /**********
-        // TODO: change string-deque to uint8_t-deque, transformation to hex is not necessary anymore here
+        // enable to print information on the recorded beacon to serial monitor
         std::stringstream stream;
         stream << std::hex << std::setfill('0');
         for (size_t i = len - 20; i < len; i++) {
@@ -54,20 +60,8 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
         }
         std::string beacon = stream.str();
         cout << "Logged beacon: " << beacon.c_str() << endl;
-
-        if (std::find(seenBeacons.begin(), seenBeacons.end(), beacon.c_str()) == seenBeacons.end()) {
-          // only last 20 bytes of interest; for 31 bytes frame, skip leading 11 bytes; for 28 bytes frame, skip leading 8 bytes
-          writeBeaconToFile(payload + (len - 20), 20);
-          seenBeacons.push_front(beacon.c_str());
-          if(seenBeacons.size() >= maxBeacons) {
-            seenBeacons.pop_back();
-          }
-        }
-        ***********/
-        // version that does not use queue to prevent duplicates
-        writeBeaconToFile(payload + (len - 20), 20);
+        **********/
       }
-      
     }
 };
 
@@ -77,7 +71,6 @@ void setup() {
   Serial.println("read - reading the catched frames log file.");
   Serial.println("log - stop/start logging of catched frames. Started on default.");
   Serial.println("---------------------------------------");
-  Serial.println("Scanning...");
 
   BLEDevice::init("");
   pBLEScan = BLEDevice::getScan();
@@ -87,7 +80,7 @@ void setup() {
   pBLEScan->setWindow(99);  // less or equal setInterval value
 
   // setup flash memory and a file to store collected beacons
-  // needs to be set to true at the first start
+  // needs to be set to true when first running this code on the sensor
   if (!SPIFFS.begin(false)) {
     Serial.println("Error while initializing SPIFFS!");
     while (true){}
@@ -96,17 +89,28 @@ void setup() {
   if (!SPIFFS.exists(beaconFile)) {
      createFile(SPIFFS, beaconFile);
   }
+
+  beaconBuf.reserve(bufSize);
 }
 
 void loop() {
   if (scan) {
+    // sleep before scanning, to avoid potential faults while storing, press reset button before disconnecting from USB
+    esp_sleep_enable_timer_wakeup(sleepTime * 1000000ULL);
+    esp_light_sleep_start();
+    
+    cout << "Scan start. One may issue commands to sensor via serial monitor now until scan complete." << endl;
     BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
-    pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+    pBLEScan->clearResults();
+    
+    cout << "Scan done. Found " << beaconBuf.size() / 23 << " beacons."<< endl;
+    writeBeaconsToFile();
   }
  
   if (Serial.available()) {
     String input = Serial.readStringUntil('\n');
     if(input.equals("read")) {
+        Serial.println("Output format: beacon;timestamp");
         readFile(SPIFFS, beaconFile);
         Serial.println("All frames displayed.");
     }    
@@ -121,31 +125,30 @@ void loop() {
       }
     }
   }
-  cout << "Scan done." << endl; 
   delay(1000);
-  esp_sleep_enable_timer_wakeup(sleepTime * 1000000ULL);
-  esp_light_sleep_start();
 }
 
-void writeBeaconToFile(uint8_t * beacon, size_t len) {
-  // seems to write in pages of 251 bytes length
-  if (SPIFFS.totalBytes() - SPIFFS.usedBytes() > 256) {
+void appendBeaconToBuffer(uint8_t* beacon, size_t beaconLen, uint8_t* metadata, size_t metadataLen) {
+  for(size_t i = 0; i < beaconLen; i++) {
+    beaconBuf.push_back(beacon[i]);
+  }
+  for(size_t j = 0; j < metadataLen; j++) {
+    beaconBuf.push_back(metadata[j]);
+  }
+}
+
+void writeBeaconsToFile() {
+  // SPIFFS seems to write in pages of 251 bytes length
+  if (SPIFFS.totalBytes() - SPIFFS.usedBytes() > beaconBuf.size()) {
     File file = SPIFFS.open(beaconFile, FILE_APPEND);
     if (file) {
-      uint64_t currentTime = esp_timer_get_time() / 1000000;
-      // 3 bytes can represent 194 days as seconds, this should be enough in our case :)
-      uint8_t timeArray[] = {(currentTime >> 16), (currentTime >> 8), currentTime};
-
-      for (size_t i = 0; i < len; i++) {
-        file.write(beacon[i]);
-      }
-      for (size_t j = 0; j < sizeof(timeArray); j++) {
-        file.write(timeArray[j]);
+      for(vector<uint8_t>::iterator it = beaconBuf.begin(); it != beaconBuf.end(); it++) {
+        file.write(*it);
       }
     }
     file.close();
-    cout << "New beacon written to file." << endl;
-    Serial.printf("Used Bytes: %d\n", SPIFFS.usedBytes());
+    beaconBuf.clear();
+    cout << "Flash memory populated with data: " << SPIFFS.usedBytes() << " bytes" << endl;
   } else {
     scan = false;
   }
