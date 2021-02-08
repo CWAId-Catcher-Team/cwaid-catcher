d41147885462d6268bc8646736f96fec3b4b2b26
Niklas Schwabe <niklas.schwabe09@gmail.com>
2021-02-08 16:09:12 +0100

    new read to align to latest BLE_catcher, some cleanup

--- src/FileIO/FileIO.ino
+++ src/FileIO/FileIO.ino
@@ -11,7 +11,7 @@ void setup() {
   delay(5000);
   if (!SPIFFS.begin(false)) {
     Serial.println("Error while initializing SPIFFS!");
-    while (true){}
+    while (true) {}
   }
   
   beaconFileExists = SPIFFS.exists(beaconFile);
@@ -20,51 +20,75 @@ void setup() {
   } else {
     Serial.println("No file with beacons found.");
   }
-  Serial.println("Waiting for input: Possible Commands: read, delete, create, clear, usedBytes, totalBytes.");
+  Serial.println("Waiting for input...");
+  listCommands();
 }
 
 void loop() {
   if (Serial.available()) {
     String input = Serial.readStringUntil('\n');
-    if(input.equals("readv2")) {
+    if (input.equals("readv1")) {
       if (beaconFileExists) {
-        Serial.println("Reading beacons from file with version 2 (beacons, timestamp):");
+        Serial.println("Output format: beacon;timestamp");
         File file = SPIFFS.open(beaconFile, FILE_READ);
-        while(file.available()){
+        while (file.available()) {
           uint8_t beacon[20];
-          uint8_t timeArray[3];
+          uint8_t metadata[3];
           file.read(beacon, sizeof(beacon));
-          file.read(timeArray, sizeof(timeArray));
+          file.read(metadata, sizeof(metadata));
           
           std::stringstream stream;
           stream << std::hex << std::setfill('0');
           for (size_t i = 0; i < sizeof(beacon); i++) {
             stream << std::hex << std::setw(2) << (unsigned int) beacon[i];
           }
-          uint32_t currentTime = (uint32_t) timeArray[0] << 16 | (uint16_t) timeArray[1] << 8 | timeArray[2];
+          uint32_t currentTime = (uint32_t) metadata[0] << 16 | (uint16_t) metadata[1] << 8 | metadata[2];
           std::cout << stream.str() << ";" << currentTime << std::endl;
         }
         file.close();
-        Serial.println("All beacons displayed with read v2 (beacons and timestamp only).");
+        Serial.println("All beacons displayed using format (beacon;timestamp).");
       } else {
         Serial.println("Beacon file does not exist.");
       }
 
-    // only use if write function only included beacons, no timestamps
-    } else if (input.equals("readv1")) {
+    } else if (input.equals("readv2")) {
       if (beaconFileExists) {
-        Serial.println("Reading beacons from file with version 1 (beacons only):");
+        Serial.println("Output format: beacon;timestamp;OS/frameFormat");
         File file = SPIFFS.open(beaconFile, FILE_READ);
-        while(file.available()){
-          Serial.write(file.read());
+        while (file.available()) {
+          uint8_t beacon[20];
+          uint8_t metadata[3];
+          file.read(beacon, sizeof(beacon));
+          file.read(metadata, sizeof(metadata));
+          
+          std::stringstream stream;
+          stream << std::hex << std::setfill('0');
+          for (size_t i = 0; i < sizeof(beacon); i++) {
+            stream << std::hex << std::setw(2) << (unsigned int) beacon[i];
+          }
+          
+          std::string formatInfo = "unexpected";
+          if (metadata[0] >= 192) {
+            metadata[0] -= 192;
+            formatInfo = "length mismatch";
+          } else if (metadata[0] >= 128) {
+            metadata[0] -= 128;
+            formatInfo = "iOS";
+          } else if (metadata[0] >= 64) {
+            metadata[0] -= 64;
+            formatInfo = "Android";
+          }
+    
+          uint32_t currentTime = (uint32_t) metadata[0] << 16 | (uint16_t) metadata[1] << 8 | metadata[2];
+          std::cout << stream.str() << ";" << currentTime << ";" << formatInfo << std::endl;
         }
         file.close();
-        Serial.println("All beacons displayed with read v1 (beacons only).");
+        Serial.println("All beacons displayed using format (beacon;timestamp;OS/frameFormat).");
       }
 
     } else if (input.equals("readv3")) {
       if (beaconFileExists) {
-        Serial.println("Reading beacons from file with version 3 (beacons, timestamp, rssi):");
+        Serial.println("Output format: beacon;timestamp;rssi");
         File file = SPIFFS.open(beaconFile, FILE_READ);
         while (file.available()) {
           uint8_t beacon[20];
@@ -81,42 +105,10 @@ void loop() {
           std::cout << stream.str() << ";" << currentTime << ";" << +int8_t(metadata[3]) << std::endl;
         }
         file.close();
-        Serial.println("All beacons displayed with read v3 (beacons, timestamp, rssi).");
+        Serial.println("All beacons displayed using format (beacon;timestamp;rssi).");
       }
 
-      
-    } else if (input.equals("customRead")) {
-      int noOfOldBeacons = 15;
-      Serial.println("Custom read from file:");
-      File file = SPIFFS.open(beaconFile, FILE_READ);
-      int count = 0;
-      while (file.available()) {
-        char c = file.read();
-        Serial.write(c);
-        if (c == '\n') {
-          count++;
-          if (count == noOfOldBeacons) {
-            break;
-          }
-        }
-      }
-      while (file.available()) {
-        uint8_t beacon[20];
-        uint8_t timeArray[3];
-        file.read(beacon, sizeof(beacon));
-        file.read(timeArray, sizeof(timeArray));
-          
-        std::stringstream stream;
-        stream << std::hex << std::setfill('0');
-        for (size_t i = 0; i < sizeof(beacon); i++) {
-          stream << std::hex << std::setw(2) << (unsigned int) beacon[i];
-        }
-        uint32_t currentTime = (uint32_t) timeArray[0] << 16 | (uint16_t) timeArray[1] << 8 | timeArray[2];
-        std::cout << stream.str() << ";" << currentTime << std::endl;
-      }
-  
     } else if (input.equals("delete")) {
-      Serial.println("Deleting beacon file:");
       Serial.printf("Deletion of beacons file successful == %s\n", SPIFFS.remove(beaconFile) ? "true" : "false");
       
     } else if (input.equals("create")) {
@@ -136,18 +128,39 @@ void loop() {
       } else {
         Serial.println("File to clear does not exist.");
       }
-      
-    } else if (input.equals("deleteAll")) {
-      Serial.println("Deleting all files:");
+       
+    } else if (input.equals("format")) {
       Serial.printf("Formatting successful == %s\n", SPIFFS.format() ? "true" : "false");
-    } else if (input.equals("usedBytes")) {
-      Serial.printf("Used SPIFFS bytes: %d\n", SPIFFS.usedBytes());
-    } else if (input.equals("totalBytes")) {
-      Serial.printf("Total SPIFFS bytes: %d\n", SPIFFS.totalBytes());
+    } else if (input.equals("usedMemory") || input.equals("used")) {
+      Serial.printf("%d bytes are populated, this is %.1f%% of the sensor's non-volatile memory.\n", SPIFFS.usedBytes(), SPIFFS.usedBytes() * 100.0f / SPIFFS.totalBytes());
+    } else if (input.equals("remainingMemory") || input.equals("remaining")) {
+      int unused = SPIFFS.totalBytes() - SPIFFS.usedBytes();
+      Serial.printf("%d bytes are unused, this is %.1f%% of the sensor's non-volatile memory.\n", unused, unused * 100.0f / SPIFFS.totalBytes());
+    } else if (input.equals("memorySize")) {
+      Serial.printf("The sensor provides %d bytes of accessible non-volatile memory.\n", SPIFFS.totalBytes());
+    } else if (input.equals("commands") || input.equals("man")) {
+      listCommands();
     } else {
       Serial.println("Invalid command.");
     }
     beaconFileExists = SPIFFS.exists(beaconFile);
   }
-  delay(2000);
+}
+
+void listCommands() {
+  Serial.println("--------------------Commands:--------------------");
+  Serial.println(" - commands: lists all possible commands that can be input to serial monitor");
+  Serial.println(" - memorySize: prints #bytes that are accessible to store data in non-volatile memory");
+  Serial.println(" - usedMemory: prints #bytes populated with data");
+  Serial.println(" - remainingMemory: prints #bytes that remains to be populated with data\n");
+
+  Serial.println(" - create: creates a file to store recorded beacons, BLE_catcher will do this itself if no file exists yet");
+  Serial.println(" - delete: deletes file that is used to store beacons");
+  Serial.println(" - format: deletes all files in file system");
+  Serial.println(" - clear: removes all recorded beacons stored on the sensor\n");
+  
+  Serial.println(" - readv1: prints data stored in memory to serial monitor, use if sensor stores (beacon;timestamp)");
+  Serial.println(" - readv2: prints data stored in memory to serial monitor, use if sensor stores (beacon;timestamp;OS/frameFormat)");
+  Serial.println(" - readv3: prints data stored in memory to serial monitor, use if sensor stores (beacon;timestamp;rssi)");
+  Serial.println("-------------------------------------------------");
 }
