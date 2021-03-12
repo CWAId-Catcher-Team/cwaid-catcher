3a8f01c20887e10786935a942944dcb911258388
maddin <mahey@hotmail.de>
2021-03-12 15:02:27 +0100

    extended count analyzer

--- src/analyzer/tools/count_analyzer.py
+++ src/analyzer/tools/count_analyzer.py
@@ -12,8 +12,17 @@ for subdir, dirnames, filenames in os.walk("../ids"):
 
 print()
 
+counter = 0
+people = 0
+ids_count = 0
+unique_ids_count = 0
+average_cwa = 0.0
+average_time = 0.0
+
 # Read files and analyze ids
 for f in files:
+    counter += 1
+
     # Parse people count from filename
     index = 0
     for c in f[1][5:]:
@@ -21,11 +30,13 @@ for f in files:
             break
         index += 1
     people_count = int(f[1][5:5+index])
+    people += people_count
     print("[" + f[1] + "] Count of people: " + str(people_count))
 
     # Read file
     with open(os.path.join(f[0], f[1]), "rb") as f_ids:
        ids = f_ids.readlines()
+    ids_count += len(ids)
     print("[" + f[1] + "] Count of all ids: " + str(len(ids)))
 
     # Find unique ids and store for each unique id all timestamps
@@ -35,14 +46,16 @@ for f in files:
             continue
         parts = i.split(b";")
         ids_unique[parts[0]].append(int(parts[1]))
+    unique_ids_count += len(ids_unique) 
     print("[" + f[1] + "] Count of all unique ids: " + str(len(ids_unique)))
 
     # Calculate percentage of unique ids to ids
-    percentage = int(100 * len(ids_unique) / len(ids))
+    percentage = int(round(100 * len(ids_unique) / len(ids), 0))
     print("[" + f[1] + "] % unique ids in all ids: " + str(percentage))
 
     # Calculate percentage of corona warn app users
-    percentage = int(100 * len(ids_unique) / people_count)
+    percentage = int(round(100 * len(ids_unique) / people_count))
+    average_cwa += 100 * (len(ids_unique) / people_count)
     print("[" + f[1] + "] % of CWA users: " + str(percentage))
 
     # Calculate average stay in sensor range for same id
@@ -55,8 +68,16 @@ for f in files:
             continue
         stay_times += v[-1] - v[0]
         stay_times_count += 1
-    avg_stay_time = int(stay_times / stay_times_count)
+    avg_stay_time = int(round(stay_times / stay_times_count, 0))
+    average_time += stay_times / stay_times_count
     print("[" + f[1] + "] Average stay time in sensor range: " + str(avg_stay_time) + " seconds")
     print()    
+
+print("[All] Count of people: " + str(people))
+print("[All] Count of all ids: " + str(ids_count))
+print("[All] Count of all unique ids: " + str(unique_ids_count))
+print("[All] Average % of CWA users: " + str(int(round(average_cwa / counter, 0))))
+#print("[All] Average stay time in sensor range: " + str(int(round(average_time / counter, 0))))
+print()
 
 print("The average stay time is sensor range is only meaningful, if the scan delay time of the sensor is short (i.e. < 10s). Also only ids that are scanned at least two times are considered. This means if a person stays longer than one scan interval in the sensors range, it will be considered in the calculation.")
